e9a2767fb6bfb71464745481c35047732fa7f991
Keobkeig <angela.xue3@gmail.com>
2021-12-15 16:47:10 -0500

    I finished it lmfao

---
+++ problem.java
@@ -0,0 +1,10 @@
+public class problem {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 694) {
+            System.out.println(a);
+            a *= 2;
+        }
+    }  
+}
+   
